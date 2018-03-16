b94419f1f36257cbeb62dc126cbd3e3600a9cb0d
Pablo Villaverde <pvillaverde@qualigy.com>
2018-03-16 01:27:56 +0100

    Feat: restaurant page responsive

--- www/app/restaurant_info.js
+++ www/app/restaurant_info.js
@@ -14,7 +14,7 @@ window.initMap = () => {
 				center: restaurant.latlng,
 				scrollwheel: false
 			});
-			fillBreadcrumb();
+			/*fillBreadcrumb();*/
 			DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
 		}
 	});
@@ -56,7 +56,6 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 	address.innerHTML = restaurant.address;
 
 	const image = document.getElementById('restaurant-img');
-	image.className = 'restaurant-img'
 	image.src = DBHelper.imageUrlForRestaurant(restaurant);
 	image.alt = `${restaurant.name} restaurant photo`;
 
@@ -75,19 +74,11 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
  * Create restaurant operating hours HTML table and add it to the webpage.
  */
 fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => {
-	const hours = document.getElementById('restaurant-hours');
+	const table = document.getElementById('restaurant-hours');
 	for (let key in operatingHours) {
 		const row = document.createElement('tr');
-
-		const day = document.createElement('td');
-		day.innerHTML = key;
-		row.appendChild(day);
-
-		const time = document.createElement('td');
-		time.innerHTML = operatingHours[key];
-		row.appendChild(time);
-
-		hours.appendChild(row);
+		row.innerHTML = `<th scope="row">${key}</th><td>${operatingHours[key].replace(/,/g,'<br>')}</td>`;
+		table.appendChild(row);
 	}
 }
 
@@ -95,57 +86,40 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  * Create all reviews HTML and add them to the webpage.
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-	const container = document.getElementById('reviews-container');
-	const title = document.createElement('h2');
-	title.innerHTML = 'Reviews';
-	container.appendChild(title);
-
+	const container = document.getElementById('reviews-list');
 	if (!reviews) {
 		const noReviews = document.createElement('p');
 		noReviews.innerHTML = 'No reviews yet!';
 		container.appendChild(noReviews);
 		return;
 	}
-	const ul = document.getElementById('reviews-list');
 	reviews.forEach(review => {
-		ul.appendChild(createReviewHTML(review));
+		container.appendChild(createReviewHTML(review));
 	});
-	container.appendChild(ul);
 }
 
 /**
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = (review) => {
-	const li = document.createElement('li');
-	const name = document.createElement('p');
-	name.innerHTML = review.name;
-	li.appendChild(name);
-
-	const date = document.createElement('p');
-	date.innerHTML = review.date;
-	li.appendChild(date);
-
-	const rating = document.createElement('p');
-	rating.innerHTML = `Rating: ${review.rating}`;
-	li.appendChild(rating);
-
-	const comments = document.createElement('p');
-	comments.innerHTML = review.comments;
-	li.appendChild(comments);
-
-	return li;
+	const article = document.createElement('article');
+	article.innerHTML = `
+		<author>${review.name}</author>
+		<time datetime="${review.date}">${review.date}</time>
+		<meter max="5" value="${review.rating}"></meter>
+		<p>${review.comments}</p>`;
+	return article;
 }
 
 /**
  * Add restaurant name to the breadcrumb navigation menu
  */
-fillBreadcrumb = (restaurant = self.restaurant) => {
+/*fillBreadcrumb = (restaurant = self.restaurant) => {
 	const breadcrumb = document.getElementById('breadcrumb');
 	const li = document.createElement('li');
 	li.innerHTML = restaurant.name;
 	breadcrumb.appendChild(li);
-}
+}*/
 
 /**
  * Get a parameter by name from page URL.
